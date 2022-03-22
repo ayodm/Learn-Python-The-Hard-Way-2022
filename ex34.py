07f63286f08092fe059cfaefe59b5aab899d1666
ayodm <ayodma@gmail.com>
2022-03-21 19:59:34 -0500

    032122

---
+++ ex34.py
@@ -0,0 +1,8 @@
+animals = ['bear', 'tiger', 'penguin','zerba']
+bear = animals[0]
+
+#list's start at 0 so to access the first element in a list you do that 
+#by calling [0] which equates to the first element.
+
+
+ 
---
+++ py_game_01.py
@@ -0,0 +1,44 @@
+from sys import exc_info 
+
+def room_1():
+    print("This is room 1")
+
+    choice = input("> ")
+    # If else statement here
+def room_2():
+    print("This is room 2")
+
+    choice = input("> ")
+    # If else statement here
+def room_3():
+    print("This is room 3")
+
+    choice = input("> ")
+    # If else statement here
+
+def room_4():
+    print("This is room 4")
+
+    choice = input("> ")
+    # If else statement here
+
+def room_5():
+    print("This is room 5")
+
+    choice = input("> ")
+    # If else statement here
+
+
+def dead(why):
+    print(why, "Good job!")
+    exit(0)
+
+def start():
+    print()
+    print()
+
+    choice = input("> ")
+
+    # If else here 
+start()
+
